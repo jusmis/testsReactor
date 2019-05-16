2ce9883632974f46e1b26775bfe802c42cadac58
jusmis <44554496+jusmis@users.noreply.github.com>
2019-05-16 17:10:28 +0200

    --New test: withdrawingAmoutThatCantBePaidWithBanknotesShouldThrowWrongMoneyAmoutException

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -9,16 +9,6 @@ import org.mockito.Mockito;
 
 public class AtmMachineTest {
 
-    /*@Mock
-    private MoneyDepot moneyDepot;
-
-    @Mock
-    private BankService bankService;
-
-    @Mock
-    private CardProviderService cardProviderService;
-    */
-
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
@@ -32,6 +22,19 @@ public class AtmMachineTest {
 
         AtmMachine atmMachine = new AtmMachine(cardProviderService, bankService, moneyDepot);
         Money money = Money.builder().withAmount(-1).withCurrency(Currency.PL).build();
+        Card card = Card.builder().withCardNumber("0").withPinNumber(1).build();
+
+        atmMachine.withdraw(money, card);
+    }
+
+    @Test(expected = WrongMoneyAmountException.class)
+    public void withdrawingAmountThatCantBePaidWithBanknotesShouldThrowWrongMoneyAmountException(){
+        CardProviderService cardProviderService = Mockito.mock(CardProviderService.class);
+        BankService bankService = Mockito.mock(BankService.class);
+        MoneyDepot moneyDepot = Mockito.mock(MoneyDepot.class);
+
+        AtmMachine atmMachine = new AtmMachine(cardProviderService, bankService, moneyDepot);
+        Money money = Money.builder().withAmount(1).withCurrency(Currency.PL).build();
         Card card = Card.builder().withCardNumber("0").withPinNumber(1).build();
 
         atmMachine.withdraw(money, card);
