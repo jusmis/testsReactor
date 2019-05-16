02548527a3dd29fb1d36739f17763eb9905d4f24
jusmis <44554496+jusmis@users.noreply.github.com>
2019-05-16 17:02:32 +0200

    --New test: withdrawigNegativeAmountOfMoneyShouldThrowWrongMoneyAmountException

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -4,12 +4,38 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 public class AtmMachineTest {
+
+    /*@Mock
+    private MoneyDepot moneyDepot;
+
+    @Mock
+    private BankService bankService;
+
+    @Mock
+    private CardProviderService cardProviderService;
+    */
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
     }
+
+    @Test(expected = WrongMoneyAmountException.class)
+    public void withdrawingNegativeAmountOfMoneyShouldThrowWrongMoneyAmountException(){
+        CardProviderService cardProviderService = Mockito.mock(CardProviderService.class);
+        BankService bankService = Mockito.mock(BankService.class);
+        MoneyDepot moneyDepot = Mockito.mock(MoneyDepot.class);
+
+        AtmMachine atmMachine = new AtmMachine(cardProviderService, bankService, moneyDepot);
+        Money money = Money.builder().withAmount(-1).withCurrency(Currency.PL).build();
+        Card card = Card.builder().withCardNumber("0").withPinNumber(1).build();
+
+        atmMachine.withdraw(money, card);
+    }
+
 
 }
